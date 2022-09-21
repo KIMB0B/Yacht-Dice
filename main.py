8d3ff32c5eadb2c2ae97133b7135b7c87ecd57a5
Jeongwook Kim <axzsd96301@gmail.com>
2022-09-21 17:02:24 +0900

    Change Play function to player class, append score

--- main.py
+++ main.py
@@ -1,53 +1,8 @@
 from player import Player
-import os, platform
-
-# 운영체제에 따라 터미널 명령어 변경
-if platform.system() == 'Windows':
-    close = 'cls'
-else:
-    close = 'clear'
-
-def play(player:Player, round:int):
-    """
-    플레이어 한명의 한 라운드를 진행합니다.
-        args :  player->Player클래스 변수의 하나의 플레이어
-                round->현재의 라운드
-    """
-    print(f"{player.name}님의 {round}라운드를 시작합니다.")
-    input()
-    turn = 1
-    player.Roll()
-    while(turn <= 3):
-        os.system(close)
-        print(f"ROUND [{round}/12]\tNAME [{player.name}]\tTURN [{turn}/3]\n\n")
-        print(f"{player.dices}\n\n")
-        print("R : 다시 굴리기, B : 점수판 보기")
-        mainCommand = input("->").upper()
-        if mainCommand == 'B': # 보드 확인 커맨드 입력 시
-            while(True):
-                os.system(close)
-                boardCommand = player.ShowBoard().upper()
-                if boardCommand == 'B': # 다시 주사위 확인
-                    break
-                elif boardCommand in player.handRankings.keys(): # 점수 입력 후 턴 종료
-                    if player.status & player.handRankings[boardCommand]['check'] == 0: # 아직 점수가 들어가지 않은 족보가 입력될 시
-                        player.status += player.handRankings[boardCommand]['check']
-                        turn = 999
-                        break
-                    else:
-                        pass
-                else: # 예외 입력, 다시 보드 확인 루프 진입
-                    pass
-        elif mainCommand == 'R': # 주사위 다시 굴리기 커맨드 입력 시
-            if turn != 3:
-                player.Roll()
-                turn += 1
-    os.system(close)
-    print(f"{player.name}님이 {player.handRankings[boardCommand]['name']}에 {player.handRankings[boardCommand]['score']}점을 넣고 {round}라운드를 종료했습니다.\n\n")
 
 player1 = Player(name="김정욱")
 player2 = Player(name="송수진")
 
 for round in range(1, 13):
-    play(player1, round)
-    play(player2, round)
+    player1.Play(round)
+    player2.Play(round)
--- player.py
+++ player.py
@@ -1,4 +1,11 @@
 import random
+import os, platform
+
+# 운영체제에 따라 터미널 명령어 변경
+if platform.system() == 'Windows':
+    close = 'cls'
+else:
+    close = 'clear'
 
 BLACK_BG = '\033[40m'
 BRIGHT_WHITE_BG = '\033[107m'
@@ -17,7 +24,20 @@ class Player:
         self.status = 0
         self.score = 0
         self.dices = list()
-        self.handRankings = {}
+        self.handRankings = {
+            "1" : {"name" : "ace"           , "check" : 0b000000000001, "score" : 0}, 
+            "2" : {"name" : "dual"          , "check" : 0b000000000010, "score" : 0},
+            "3" : {"name" : "triple"        , "check" : 0b000000000100, "score" : 0},
+            "4" : {"name" : "quad"          , "check" : 0b000000001000, "score" : 0},
+            "5" : {"name" : "penta"         , "check" : 0b000000010000, "score" : 0},
+            "6" : {"name" : "hexa"          , "check" : 0b000000100000, "score" : 0},
+            "7" : {"name" : "choice"        , "check" : 0b000001000000, "score" : 0},
+            "8" : {"name" : "poker"         , "check" : 0b000010000000, "score" : 0},
+            "9" : {"name" : "fullHouse"     , "check" : 0b000100000000, "score" : 0},
+            "0" : {"name" : "smallStraight" , "check" : 0b001000000000, "score" : 0},
+            "-" : {"name" : "largeStraight" , "check" : 0b010000000000, "score" : 0},
+            "=" : {"name" : "yacht"         , "check" : 0b100000000000, "score" : 0}
+        }
 
     def Roll(self):
         """ 
@@ -28,21 +48,15 @@ class Player:
         self.dices.clear()
         for i in range(0, 5):
             self.dices.append(random.randint(1, 6))
-        
-        self.handRankings = {
-            "1" : {"name" : "ace"           , "check" : 0b000000000001, "score" : len([i for i in self.dices if i == 1])*1}, 
-            "2" : {"name" : "dual"          , "check" : 0b000000000010, "score" : len([i for i in self.dices if i == 2])*2},
-            "3" : {"name" : "triple"        , "check" : 0b000000000100, "score" : len([i for i in self.dices if i == 3])*3},
-            "4" : {"name" : "quad"          , "check" : 0b000000001000, "score" : len([i for i in self.dices if i == 4])*4},
-            "5" : {"name" : "penta"         , "check" : 0b000000010000, "score" : len([i for i in self.dices if i == 5])*5},
-            "6" : {"name" : "hexa"          , "check" : 0b000000100000, "score" : len([i for i in self.dices if i == 6])*6},
-            "7" : {"name" : "choice"        , "check" : 0b000001000000, "score" : sum(self.dices)},
-            "8" : {"name" : "poker"         , "check" : 0b000010000000, "score" : 0},
-            "9" : {"name" : "fullHouse"     , "check" : 0b000100000000, "score" : 0},
-            "0" : {"name" : "smallStraight" , "check" : 0b001000000000, "score" : 0},
-            "-" : {"name" : "largeStraight" , "check" : 0b010000000000, "score" : 0},
-            "=" : {"name" : "yacht"         , "check" : 0b100000000000, "score" : 0}
-        }
+
+        ## 점수가 아직 입력되지 않은 족보의 보드판에서 점수 확인 ##
+        for key in self.handRankings:
+            if self.handRankings[key]['check'] & self.status == 0:
+                if key == '1' or key == '2' or key == '3' or key == '4' or key == '5' or key == '6': # ACE ~ HEXA 보드판 점수 확인
+                    self.handRankings[key].update({'score' : len([i for i in self.dices if i == int(key)])*int(key)})
+                elif key == '7': # CHOICE 보드판 점수 확인
+                    self.handRankings[key].update({'score' : sum(self.dices)})
+        ## 점수가 아직 입력되지 않은 족보의 점수 확인 ##
     
     def ShowBoard(self):
         """ 
@@ -59,3 +73,50 @@ class Player:
         print(BRIGHT_WHITE_BG+BLACK+"└------------------┴------┘"+END)
         print("\nB : 돌아가기, 1 ~ = : 점수 입력")
         return input("->")
+
+    def Play(self, round:int):
+        """
+        플레이어 한명의 한 라운드를 진행합니다.
+            args :  round->현재의 라운드
+        """
+        print(f"{self.name}님의 {round}라운드를 시작합니다.")
+        input()
+        turn = 1
+        self.Roll()
+        while(turn <= 3):
+            os.system(close)
+            print(f"ROUND [{round}/12]\tNAME [{self.name}]\tTURN [{turn}/3]\n\n")
+            print(f"{self.dices}\n\n")
+            print("R : 다시 굴리기, B : 점수판 보기")
+            mainCommand = input("->").upper()
+            if mainCommand == 'B': # 보드 확인 커맨드 입력 시
+                while(True):
+                    os.system(close)
+                    boardCommand = self.ShowBoard().upper()
+
+                    if boardCommand == 'B': # 다시 주사위 확인
+                        break
+
+                    elif boardCommand in self.handRankings.keys(): # 족보 key 입력 시
+                        ## 점수가 아직 입력되지 않은 족보의 점수 입력 ##
+                        if self.status & self.handRankings[boardCommand]['check'] == 0:
+                            self.status += self.handRankings[boardCommand]['check']
+                            if boardCommand == '1' or boardCommand == '2' or boardCommand == '3' or boardCommand == '4' or boardCommand == '5' or boardCommand == '6': # ACE ~ HEXA 점수 입력
+                                self.handRankings[boardCommand].update({'score' : len([i for i in self.dices if i == int(boardCommand)])*int(boardCommand)})
+                            elif boardCommand == '7': # CHOICE 점수 입력
+                                self.handRankings[boardCommand].update({'score' : sum(self.dices)})
+                            
+                            turn = 999
+                            break
+                        ## 점수가 아직 입력되지 않은 족보의 점수 입력 ##
+                        else: # 이미 점수가 입력된 족보일 시 pass
+                            pass
+
+                    else: # 예외 입력 시 다시 보드 확인 루프 진입
+                        pass
+            elif mainCommand == 'R': # 주사위 다시 굴리기 커맨드 입력 시
+                if turn != 3:
+                    self.Roll()
+                    turn += 1
+        os.system(close)
+        print(f"{self.name}님이 {self.handRankings[boardCommand]['name']}에 {self.handRankings[boardCommand]['score']}점을 넣고 {round}라운드를 종료했습니다.\n\n")
